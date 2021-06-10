3fd7c5a619022b708a22595896c119c93cd30ea2
teddyaugust <loyalcoachsystem@gmail.com>
2021-06-10 16:11:05 +0700

    Version Name 3. Version Code 2.0.2

---
+++ app/src/main/java/com/teddy/gerejayesus20/KontakGereja.java
@@ -0,0 +1,162 @@
+package com.teddy.gerejayesus20;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class KontakGereja extends AppCompatActivity {
+
+    //Inisialiasi Variabel
+    DrawerLayout drawerLayout;
+    WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kontak_gereja);
+
+        //buka webnya
+        webView = (WebView) findViewById(R.id.kontak_gereja_webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://gereja.yuk-simak.info/layanan-gereja/");
+
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDisplayZoomControls(false);
+        webSettings.supportZoom();
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        webView.setWebChromeClient(new WebChromeClient());
+
+        webView.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView wv, String url) {
+                if(url.startsWith("tel:") || url.startsWith("whatsapp:") || url.startsWith("tg:") || url.startsWith("facebook:") ) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+
+        //Assign variabel
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void ClickMenu (View view){
+        //Open drawer
+        MainActivity.openDrawer(drawerLayout);
+    }
+
+    public void ClickLogo (View view){
+        //Close drawer
+        MainActivity.closeDrawer(drawerLayout);
+    }
+
+    public void ClickHome (View view){
+        //Redirect Activity to home
+        MainActivity.redirectActivity(this,MainActivity.class);
+    }
+
+    public void ClickRenungan (View view){
+        //Redirect Activity to home
+        MainActivity.redirectActivity(this,Renungan.class);
+    }
+
+    public void ClickIbadahOnline (View view){
+        //Redirect activity ke Ibadah Online
+        MainActivity.redirectActivity(this,IbadahOnline.class);
+    }
+
+    public void ClickWartaJemaat (View view){
+        //Redirect activity ke Warta Jemaat
+        MainActivity.redirectActivity(this,WartaJemaat.class);
+    }
+
+    public void ClickWartaPengurus (View view){
+        //Redirect activity ke Warta Pengurus Komisi
+        MainActivity.redirectActivity(this,WartaPengurus.class);
+    }
+
+    public void ClickProfilGereja (View view){
+        //Redirect activity ke ProfilGereja
+        MainActivity.redirectActivity(this,ProfilGereja.class);
+    }
+
+    public void ClickJemaat (View view){
+        //Redirect activity ke Cara Menjadi Jemaat
+        MainActivity.redirectActivity(this,MenjadiJemaat.class);
+    }
+
+    public void ClickKontak (View view){
+        //Recreate Activity
+        recreate();
+    }
+
+    public void ClickBukuTamu (View view){
+        //Redirect activity ke BukuTamu
+        MainActivity.redirectActivity(this,BukuTamu.class);
+    }
+
+    public void ClickChatBotWA (View view){
+        //Redirect activity ke ChatBotWA
+        MainActivity.redirectActivity(this,ChatBotWA.class);
+    }
+
+    public void ClickAlkitabOnline (View view){
+        //Redirect activity ke Alkitab Online
+        MainActivity.redirectActivity(this,AlkitabOnline.class);
+    }
+
+    public void ClickKidungOnline (View view){
+        //Redirect activity ke Kidung Online
+        MainActivity.redirectActivity(this,KidungOnline.class);
+    }
+
+    public void ClickTentang (View view){
+        //Redirect activity ke Tentang Aplikasi
+        MainActivity.redirectActivity(this,TentangAplikasi.class);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Close drawer
+        MainActivity.closeDrawer(drawerLayout);
+    }
+
+}
---
+++ app/src/main/java/com/teddy/gerejayesus20/WartaJemaat.java
@@ -0,0 +1,119 @@
+package com.teddy.gerejayesus20;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WartaJemaat extends AppCompatActivity {
+
+    //Inisialiasi Variabel
+    DrawerLayout drawerLayout;
+    WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_warta_jemaat);
+
+        //buka webnya
+        webView = (WebView) findViewById(R.id.warta_jemaat_webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://gereja.yuk-simak.info/category/warta/warta-2021/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        //Assign variabel
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void ClickMenu (View view){
+        //Open drawer
+        MainActivity.openDrawer(drawerLayout);
+    }
+
+    public void ClickLogo (View view){
+        //Close drawer
+        MainActivity.closeDrawer(drawerLayout);
+    }
+
+    public void ClickHome (View view){
+        //Redirect Activity to home
+        MainActivity.redirectActivity(this,MainActivity.class);
+    }
+
+    public void ClickRenungan (View view){
+        //Redirect Activity to home
+        MainActivity.redirectActivity(this,Renungan.class);
+    }
+
+    public void ClickIbadahOnline (View view){
+        //Redirect activity ke Ibadah Online
+        MainActivity.redirectActivity(this,IbadahOnline.class);
+    }
+
+    public void ClickWartaJemaat (View view){
+        //Recreate Activity
+        recreate();
+    }
+
+    public void ClickWartaPengurus (View view){
+        //Redirect activity ke Warta Pengurus Komisi
+        MainActivity.redirectActivity(this,WartaPengurus.class);
+    }
+
+    public void ClickProfilGereja (View view){
+        //Redirect activity ke ProfilGereja
+        MainActivity.redirectActivity(this,ProfilGereja.class);
+    }
+
+    public void ClickJemaat (View view){
+        //Redirect activity ke Cara Menjadi Jemaat
+        MainActivity.redirectActivity(this,MenjadiJemaat.class);
+    }
+
+    public void ClickKontak (View view){
+        //Redirect activity ke Kontak Gereja
+        MainActivity.redirectActivity(this,KontakGereja.class);
+    }
+
+    public void ClickBukuTamu (View view){
+        //Redirect activity ke BukuTamu
+        MainActivity.redirectActivity(this,BukuTamu.class);
+    }
+
+    public void ClickChatBotWA (View view){
+        //Redirect activity ke ChatBotWA
+        MainActivity.redirectActivity(this,ChatBotWA.class);
+    }
+
+    public void ClickAlkitabOnline (View view){
+        //Redirect activity ke Alkitab Online
+        MainActivity.redirectActivity(this,AlkitabOnline.class);
+    }
+
+    public void ClickKidungOnline (View view){
+        //Redirect activity ke Kidung Online
+        MainActivity.redirectActivity(this,KidungOnline.class);
+    }
+
+    public void ClickTentang (View view){
+        //Redirect activity ke Tentang Aplikasi
+        MainActivity.redirectActivity(this,TentangAplikasi.class);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Close drawer
+        MainActivity.closeDrawer(drawerLayout);
+    }
+
+}
